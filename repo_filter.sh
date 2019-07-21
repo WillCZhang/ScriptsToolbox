b8ae6d2be9d777faf66ca376b71b3748d75637ce
WillCZhang <will.chi.zhang@gmail.com>
2019-07-20 22:21:00 -0700

    added error handling

--- repo_filter.sh
+++ repo_filter.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set -eux
-# This script filter and downloads the repos based on the desired language
+# This script filters the repos based on the desired language
 
 # Config
 # desired language to look for
@@ -8,10 +7,13 @@ DESIRED_LANGUAGE="Java"
 # a text file contains a list of repo link (e.g. https://github.com/WillCZhang/ScriptsToolbox) to check, 
 REPO_LIST_FILE_NAME="repolist.txt"
 # should the desired language be the main language in this repo?
-# false (or everything else): this repo contains the desired language
 # true: the desired language should be the main language used in this repo
+# false (or everything else): this repo contains the desired language
 IS_MAJORITY=true
-OUTPUT="result.txt"
+# output the result repo list to
+OUTPUT="result_$(date +"%s").txt"
+# output error repos to
+ERROR="result_error_$(date +"%s").txt"
 
 # Setup workdir
 RESULT_FOLDER="result_repo_list_$(date +"%s")"
@@ -31,19 +33,27 @@ else
   echo "puts project.languages"                                       >> "$LANGUAGE_ANALYZER_SCRIPT"
 fi
 
+# Start Processing
 # use both username & repo to avoid duplicate repo names
 REPO_NAMES=$(awk -F '/' '{print $4 "/" $5}' "$REPO_LIST_FILE_NAME")
 for REPO_NAME in $REPO_NAMES; do
+  echo "Current result status:"
+  cat "$OUTPUT"
+  echo "Start processing https://github.com/$REPO_NAME.git"
   REPO_FOLDER_NAME="${REPO_NAME/\//-}"
-  git clone --recurse-submodules "https://github.com/$REPO_NAME.git" "$REPO_FOLDER_NAME"
+  git clone "https://github.com/$REPO_NAME.git" "$REPO_FOLDER_NAME"
+  if [ ! $? -eq 0 ]; then
+    echo "https://github.com/$REPO_NAME.git" >> "$ERROR"
+    continue
+  fi
   cp "$LANGUAGE_ANALYZER_SCRIPT" "$REPO_FOLDER_NAME"
   cd "$REPO_FOLDER_NAME"
-  # Analyze the repo language structure
+  # analyze the repo language structure
   RESULT=$(ruby "$LANGUAGE_ANALYZER_SCRIPT" | grep "$DESIRED_LANGUAGE" | wc -l)
   cd ..
-  # Remove the repo the save space
-  rm -rf "$REPO_FOLDER_NAME"
   if [ ! $RESULT -eq 0 ]; then
     echo "https://github.com/$REPO_NAME" >> "$OUTPUT"
   fi
+  # remove the repo the save space
+  rm -rf "$REPO_FOLDER_NAME"
 done
